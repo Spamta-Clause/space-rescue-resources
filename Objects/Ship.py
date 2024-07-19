ecd78817e3d569dcc5faf60b336784f58a51b975
Spamta <111825383+Spamta-Clause@users.noreply.github.com>
2024-07-19 14:03:09 +1000

    Force ship into bounds
    Also actually works now.
    https://damom73.github.io/space-rescue-tutorials/08_ship_in_bounds.html

--- Objects/Ship.py
+++ Objects/Ship.py
@@ -1,4 +1,4 @@
-from GameFrame import RoomObject
+from GameFrame import RoomObject, Globals
 import pygame
 from colorama import Fore, Style
 
@@ -31,29 +31,45 @@ class Ship(RoomObject):
             Respond to keypress up and down
             """
 
-            if key[pygame.K_W]:
+            if key[pygame.K_w]:
                 self.y_speed = -10
-            if key[pygame.K_S]:
+            if key[pygame.K_s]:
                 self.y_speed = 10
         elif movementMethod == "while":
             """
             Respond to keypress up and down
             """
 
-            if key[pygame.K_W]:
+            if key[pygame.K_w]:
                 self.y_speed -= 10
-            elif key[pygame.K_S]:
+            elif key[pygame.K_s]:
                 self.y_speed += 10
         elif movementMethod == "acceleration":
             """
             Respond to keypress up and down
             """
 
-            if key[pygame.K_W]:
+            if key[pygame.K_w]:
                 self.y_speed -= 5
-            elif key[pygame.K_S]:
+            elif key[pygame.K_s]:
                 self.y_speed += 5
         
         else:
             print(Fore.RED + Style.BRIGHT + "Movement method not recognized" + Style.RESET_ALL)
+    
+    def keep_in_room(self):
+        """
+        Keeps the ship inside the room
+        """
+
+        if self.y <0:
+            self.y = 0
+        elif self.y + self.height > Globals.SCREEN_HEIGHT:
+            self.y = Globals.SCREEN_HEIGHT - self.height
+    
+    def step(self):
+        """
+        Determine what happens to the Ship on each click of the game clock
+        """
+        self.keep_in_room()
     
