53e780a0ebc5c9465c4506b654ac1331aa6f1963
Spamta <111825383+Spamta-Clause@users.noreply.github.com>
2024-07-23 14:32:12 +1000

    Zork
    Zork has randomized movements.

--- Objects/Zork.py
+++ Objects/Zork.py
@@ -1,4 +1,5 @@
-from GameFrame import RoomObject
+from GameFrame import RoomObject, Globals
+import random
 
 class Zork(RoomObject):
     """
@@ -15,3 +16,18 @@ class Zork(RoomObject):
         #set image
         image = self.load_image("Zork.png")
         self.set_image(image, 135, 165)
+
+        #set initial movement
+        self.y_speed = random.choice([-10, 10])
+    def keep_in_room(self):
+        """
+        Keep the Zork object in the room
+        """
+        if self.rect.top < 0 or self.y > Globals.SCREEN_HEIGHT - self.height:
+            self.y_speed *= -1
+    
+    def step(self):
+        """
+        1 per frame
+        """
+        self.keep_in_room()
